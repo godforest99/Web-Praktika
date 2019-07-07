0f66a88c89f17ec681c1c9ae41ba9d5f098df60f
jawil003 <jw19980309@gmail.com>
2019-07-07 16:41:21 +0200

    Praktikum 10 Aufgabe 1 (Begonnen)

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ function getViewportWidth() {
   return window.innerWidth || document.documentElement.clientWidth;
 }
 
+function Seminar(titel, name, ort,startZeit, startDatum, endZeit, endDatum,
+   freiePlaetze, gesamtePlaetze, tutoren) {
+    this.titel=titel;
+    this.name=name;
+    this.ort=ort;
+    this.startzeit=new Date(startDatum, startZeit);
+    this.endzeit=new Date(endDatum, endZeit);
+    this.freiePlaetze=freiePlaetze;
+    this.gesamtePlaetze=gesamtePlaetze;
+    this.tutoren=Array.of(tutoren);
+  }
+
 console.log(`Die Viewport Breite beträgt ${getViewportWidth()} Pixel.`);
