9d025c1097e1f584b7cd2fdd56070c612a5224bb
godforest99 <Kev-Gottwald@gmx.de>
2019-07-08 11:39:53 +0200

    Praktikum 10 (Datum fast gefixt, jannik mach du mal)

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function getViewportWidth() {
   return window.innerWidth || document.documentElement.clientWidth;
 }
 
-function Seminar(titel, name, ort,startZeit, startDatum, endZeit, endDatum,
+function Seminar(titel, name, ort,startzeit, endzeit,
    freiePlaetze, gesamtePlaetze, tutoren) {
     this.titel=titel;
     this.name=name;
     this.ort=ort;
-    this.startzeit=new Date(startDatum, startZeit);
-    this.endzeit=new Date(endDatum, endZeit);
+    this.startzeit=new Date(startzeit);
+    this.endzeit=new Date(endzeit);
     this.freiePlaetze=freiePlaetze;
     this.gesamtePlaetze=gesamtePlaetze;
     this.tutoren=Array.of(tutoren);
     this.getBplaetze = function(){return this.gesamtePlaetze - this.freiePlaetze};
   }
   
-var seminar1 = new Seminar("Test", "aha", "dortmund", 15, 05-05-2019, 18, 05-05-2019, 10, 22, "amk1")
-var seminar2 = new Seminar("Test2", "ahja", "dortmund", 13, 06-06-2019, 15, 05-05-2019, 5, 20,"bruh")
+var seminar1 = new Seminar("Test", "aha", "dortmund",new Date(2019,01,20,12,00), new Date(2019,01,20,14,00), 10, 22, "amk1")
+var seminar2 = new Seminar("Test2", "ahja", "dortmund", new Date(2019,02,20,12,00), new Date(2019,02,20,15,00), 5, 20,"bruh")
 let seminare = new Array(2);
 seminare.push(seminar1, seminar2);
 seminare.sort(function(x, y){ return x-y;})
