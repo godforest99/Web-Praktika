1e10e3f10434819ce7da49adfe61cf0156ac3732
godforest99 <Kev-Gottwald@gmx.de>
2019-07-09 18:15:25 +0200

    Praktikum 11 ( Aufgabe 3 müsstest du mal selber testen)

--- script.js
+++ script.js
@@ -15,13 +15,40 @@ function Seminar(titel, name, ort,startzeit, endzeit,
     this.getBplaetze = function(){return this.gesamtePlaetze - this.freiePlaetze};
   }
   
-var seminar1 = new Seminar("Test", "aha", "dortmund",new Date(2019,01-1,20,12,00), new Date(2019,01,20,14,00), 10, 22, "amk1")
-var seminar2 = new Seminar("Test2", "ahja", "dortmund", new Date(2019,02-1,20,12,00), new Date(2019,02,20,15,00), 5, 20,"bruh")
+var seminar1 = new Seminar("Unternehmensgründung", "Unternehmensgründung", "dortmund",new Date(2019,01-1,20,12,00), new Date(2019,01,20,14,00), 10, 22, "amk1")
+var seminar2 = new Seminar("Unternehmensgründung", "Unternehmensgründung", "dortmund", new Date(2019,02-1,20,12,00), new Date(2019,02,20,15,00), 5, 20,"bruh")
 let seminare = new Array(2);
 seminare.push(seminar1, seminar2);
 seminare.sort(function(x, y){ return x-y;})
 
 console.log(`Die Viewport Breite beträgt ${getViewportWidth()} Pixel.`);
-for(let i=0; i < seminare.length; i++){
-  console.log(`${seminare[i].titel} ( ${seminare[i].startzeit.toLocaleDateString()} ${seminare[i].getBplaetze()} von ${seminare[i].gesamtePlaetze} belegt)`);
+seminare.forEach(function (seminar){
+  console.log(`${seminar.titel} ( ${seminar.startzeit.toLocaleDateString()} ${seminar.getBplaetze()} von ${seminar.gesamtePlaetze} belegt)`);
 }
+);
+let createTd = function(content) {
+  let td = document.createElement("td");
+  td.textContent = content;
+  return td;
+}
+let createTda = function(content) {
+  let td = document.createElement("td");
+  td.append(content);
+  return td;
+}
+let tbody = document.querySelector("tbody");
+
+seminare.forEach (seminar=>{
+let tr = document.createElement("tr");
+let a = document.createElement("a");
+a.href = "DetailseiteSeminar.html";
+a.textContent = seminar.name;
+
+tr.append(createTda(a), createTd(seminar.startzeit.toLocaleDateString()), createTd(seminar.ort))
+tbody.append(tr);
+});
+
+
+
+
+
---
+++ tictac.js
@@ -0,0 +1,14 @@
+for(let i=1;i<101;i++){
+   if(i%3==0&&i%5==0){
+        return "TicTac";
+   }
+   
+   
+    if(i%3==0) {
+        return"Tic";
+    
+    }
+    if(i%5==0) {
+        return "Tac";
+    }
+}
